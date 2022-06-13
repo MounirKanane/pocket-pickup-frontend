470a41d8009ac637d458fc26e34048e9eada6353
ZMTakriti <71720717+ZMTakriti@users.noreply.github.com>
2022-06-13 18:00:38 +0300

    Set up MapView component
    Set up the open source Expo MapView component in the HomeScreen component

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -1,14 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
+import React from 'react';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style= {styles.container}>
+      <MapView style = {styles.map}/>
     </View>
   )
 }
 
 export default HomeScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
