fe4c7dbda8987f8a56a779921a18bae78d6f4cb1
yos497 <75061482+yos497@users.noreply.github.com>
2022-06-22 20:08:33 +0300

    CreateEvent screen created

---
+++ app/screens/EventCreationScreen.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { ImageBackground, KeyboardAvoidingView, StyleSheet, Text } from "react-native";
+
+
+const LogInScreen = () => {
+    return (
+    // Ensures that user keyboard does not block input fields
+    <KeyboardAvoidingView style={styles.background}> 
+        <Text>
+            Hello
+        </Text>
+    </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    background: {
+        flex: 1,
+        justifyContent: 'center'
+    }
+});
+
+export default LogInScreen;
