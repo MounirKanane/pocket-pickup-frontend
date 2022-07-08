22d6d5c730db1b0057e494b05f9a85d19aa7aa76
ZMTakriti <71720717+ZMTakriti@users.noreply.github.com>
2022-07-08 15:03:20 +0300

    Add GooglePlacesAutocomplete component

--- app/screens/HomeScreen.tsx
+++ app/screens/HomeScreen.tsx
@@ -2,12 +2,17 @@ import { StyleSheet, Text, View, Dimensions, Button, ScrollView } from 'react-na
 import MapView from 'react-native-maps';
 import React from 'react';
 import BottomSheet from '../components/BottomSheet';
+// import CircleButton from 'react-native-circle-button';
+import GooglePlacesInput from '../components/GooglePlacesInput'
 
 const HomeScreen = () => {
   return (
     <View style= {styles.container}>
-      <Button title="Create event" onPress = { () => console.log("awesome")} />
       <MapView style={styles.map}/>
+      <Button title="Create event" onPress = { () => console.log("awesome")} />
+      {/* <View style={{ flex: 1 }}>
+                <CircleButton size={45} />
+      </View> */}
 
       <BottomSheet>
         <ScrollView>
@@ -23,7 +28,7 @@ export default HomeScreen
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    height: Dimensions.get('window').height,
+    height: Dimensions.get('screen').height,
     width: Dimensions.get('screen').width,
     flex: 1,
     backgroundColor: '#fff',
